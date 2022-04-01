bb9cebb0710cd46a0ea7c3fc32fe880c187eae75
1842501760@qq.com <1842501760@qq.com>
2022-04-01 21:22:38 +0800

    "start"

---
+++ demo01/src/main/java/com/dyw/fdfs/App.java
@@ -0,0 +1,15 @@
+package com.dyw.fdfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Devil
+ * @create 2022-04-01 21:21
+ */
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class,args);
+    }
+}
